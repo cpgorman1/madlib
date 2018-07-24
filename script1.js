6857be282a9cde064f676f70b4833448a467a2d7
DePaul <CONF0576@depaul.edu>
2018-07-24 15:27:35 -0500

    first commit

---
+++ script1.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+        $("p").html("The #noun and the .");
+ 
+	 var noun = $("#noun").val();
+	 console.log(noun); 
+	
+	 var Noun = $("#noun2").val();
+	 console.log(Noun);
+	
+	
+	 var verb = $("#verb").val();
+	 console.log(verb);
+	 
+	 var Madlib = $("#Madlib").val();
+	 console.log(Madlib);
+	 
+	 
+
+});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
